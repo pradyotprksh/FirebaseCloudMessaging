c020e79fe422dcb07cd861615cc7620bdc7a2088
3Embed <pradyotprksh4@gmail.com>
2018-12-10 18:07:49 +0530

    Refactor: Random Commit

--- app/src/main/java/com/example/a3embed/firebasecloudmessaging/MainActivity.java
+++ app/src/main/java/com/example/a3embed/firebasecloudmessaging/MainActivity.java
@@ -1,7 +1,28 @@
 package com.example.a3embed.firebasecloudmessaging;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.UUID;
 
 /**
  * MainActivity.java
@@ -12,10 +33,64 @@ import android.os.Bundle;
  */
 public class MainActivity extends AppCompatActivity {
 
+    private static final String CHANNEL_ID = "CUSTOM_ID";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView dateTv = findViewById(R.id.dateTv);
+        Button getNotification = findViewById(R.id.getNotification);
+        createNotificationChannel();
+
+        Calendar cal = Calendar.getInstance();
+        @SuppressLint("SimpleDateFormat") SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+        dateTv.setText(sdf.format(cal.getTime()));
+
+        final TelephonyManager tm = (TelephonyManager) getBaseContext().getSystemService(Context.TELEPHONY_SERVICE);
+
+        final String tmDevice, tmSerial, androidId;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        tmDevice = "" + tm.getDeviceId();
+        tmSerial = "" + tm.getSimSerialNumber();
+        androidId = "" + android.provider.Settings.Secure.getString(getContentResolver(), android.provider.Settings.Secure.ANDROID_ID);
+
+        UUID deviceUuid = new UUID(androidId.hashCode(), ((long)tmDevice.hashCode() << 32) | tmSerial.hashCode());
+        String deviceId = deviceUuid.toString();
+
+        Toast.makeText(MainActivity.this, deviceId, Toast.LENGTH_SHORT).show();
+
+        getNotification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(MainActivity.this, CHANNEL_ID);
+                mBuilder.setSmallIcon(R.drawable.ic_launcher_background);
+                mBuilder.setContentTitle("Message");
+                mBuilder.setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText("This is a local message from the device"));
+                mBuilder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(MainActivity.this);
+                notificationManager.notify(2, mBuilder.build());
+            }
+        });
+
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "CHANNEL_NAME";
+            String description = "CHANNEL_DESCRIPTION";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 
 }
